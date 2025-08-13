a7fb3025fed01895f012b328ca559b34b7f867ad
Rishi Jha <rishijha2312@gmail.com>
2025-08-13 21:34:17 +0530

    feat: throw error for negative numbers

--- src/utils/stringCalculator.test.js
+++ src/utils/stringCalculator.test.js
@@ -18,3 +18,6 @@ test("handles new lines as delimiters", () => {
 test("supports custom delimiter", () => {
   expect(add("//;\n1;2")).toBe(3);
 });
+test("throws error for negative numbers", () => {
+  expect(() => add("1,-2,3,-4")).toThrow("negative numbers not allowed -2,-4");
+});
