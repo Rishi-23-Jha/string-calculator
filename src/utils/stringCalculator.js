194103f3062308c25b4e361b75bc18bcd5d55cfb
Rishi Jha <rishijha2312@gmail.com>
2025-08-13 21:29:43 +0530

    feat: sum of two or more numbers

--- src/utils/stringCalculator.js
+++ src/utils/stringCalculator.js
@@ -1,5 +1,6 @@
 export function add(numbers) {
   if (!numbers) return 0;
   const nums = numbers.split(",");
-  if (nums.length === 1) return parseInt(nums[0]);
+  const parsed = nums.map(Number);
+  return parsed.reduce((a, b) => a + b, 0);
 }
